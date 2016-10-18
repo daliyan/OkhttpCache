ebff698da68203431f0681a1d2f48314629fb70a
zhiwu_yan <dali_yan@yeah.net>
2016-10-18 23:32:33 +0800

    修改工程名称

---
+++ sample/src/main/java/akiyama/okhttpcache/Avatar.java
@@ -0,0 +1,17 @@
+package akiyama.okhttpcache;
+
+/**
+ * Created by Administrator on 2016/10/17.
+ */
+public class Avatar {
+
+    /**
+     * avatar": {
+     "id": "973ab4cf681688decb6f8dfa9390ee4d",
+     "template": "https://pic2.zhimg.com/{id}_{size}.jpeg"
+     }
+     */
+
+    public String id;
+    public String template;
+}
